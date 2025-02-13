abcc272269a7a2e1c45f14cc0467c5be9e3af8dd
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2025-02-13 12:39:21 -0500

    Better LineageOS bring-up

--- lineage_waffle.mk
+++ lineage_waffle.mk
@@ -8,20 +8,44 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from waffle device
-$(call inherit-product, device/oneplus/waffle/device.mk)
+# Inherit device configuration
+DEVICE_CODENAME := waffle
+DEVICE_PATH := device/oneplus/waffle
+COMMON_DEVICE_PATH := device/oneplus/sm8650-common
+VENDOR_PATH := vendor/oneplus/waffle
+COMMON_VENDOR_PATH := vendor/oneplus/sm8650-common
+$(call inherit-product, $(DEVICE_PATH)/device.mk)
 
-# Inherit some common Lineage stuff.
+# Inherit some common stuff
+ROM_VENDOR := lineage
+ifdef ROM_VENDOR
+$(call inherit-product, vendor/$(ROM_VENDOR)/config/common_full_phone.mk)
+else
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+endif
 
-PRODUCT_NAME := lineage_waffle
-PRODUCT_DEVICE := waffle
+# Device identifier. This must come after all inclusions
+ifdef ROM_VENDOR
+PRODUCT_NAME := $(ROM_VENDOR)_$(DEVICE_CODENAME)
+else
+PRODUCT_NAME := lineage_$(DEVICE_CODENAME)
+endif
+PRODUCT_DEVICE := $(DEVICE_CODENAME)
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
 PRODUCT_MODEL := CPH2581
 
-PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+PRODUCT_AAPT_CONFIG := xxxhdpi
+PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
+PRODUCT_CHARACTERISTICS := nosdcard
 
+# Boot animation
+scr_resolution := 1440
+TARGET_SCREEN_HEIGHT := 3120
+TARGET_SCREEN_WIDTH := 1440
+TARGET_BOOT_ANIMATION_RES := 1440
+
+# Build info
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="CPH2581EEA-user 15 AP3A.240617.008 U.R4T3.1c799fe_ca44_2 release-keys" \
     BuildFingerprint=OnePlus/CPH2581EEA/OP595DL1:15/AP3A.240617.008/U.R4T3.1c799fe_ca44_2:user/release-keys \
@@ -29,3 +53,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     DeviceProduct=CPH2581 \
     SystemDevice=OP595DL1 \
     SystemName=CPH2581
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
